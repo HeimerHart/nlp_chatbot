0af90a4f364e0aabfc1c51eac7b6d18bc20367ae
HeimerHart <s.sriansh0405@gmail.com>
2026-05-22 00:09:21 +0530

    chatbot frontend 1.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
 const App=()=>{
   return(
     <div className="Container">
-      <div className="chatbot=popup">
+      <div className="chatbotpopup">
         <div className="chat-header">
           <div className="header-info">
             <h2 className="logo-txt">ChatBot</h2>
 
             {/*chat body*/}
-            
+            <div className="chatbody">
+              <div className="botmessage">
+                <p className="messagetext">
+                  Good Day!<br/>
+                  How can I help you
+                </p>
+
+              </div>
+
+              <div className="usermessage">
+                nigga nigga nigga
+              </div>
+
+            </div>
 
 
 
@@ -15,8 +28,13 @@ const App=()=>{
             {/*chat footer */}
             <div className="chatfooter">
               <form action="#" className="chatform">
-                <input type="text" placeholder="Message: " className="messageinput" required>
-                </input>
+                <input type="text" placeholder="Message: " className="messageinput" required />
+                <button className="materials-symbols-rounded"><span class="material-symbols-outlined">
+                    send
+                  </span>
+                  </button>
+                  
+                                  
 
               </form>
             </div>

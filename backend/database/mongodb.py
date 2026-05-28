626339664f91df617795b2a05d1bc5ae4355e09c
HeimerHart <s.sriansh0405@gmail.com>
2026-05-29 00:58:51 +0530

    database working

--- backend/database/mongodb.py
+++ backend/database/mongodb.py
@@ -1,5 +1,25 @@
 from pymongo import MongoClient
-from dotenv import Load_dotenv
+from dotenv import load_dotenv
 import os
+from pathlib import Path
 
+# Find backend folder
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load .env from backend folder
+load_dotenv(BASE_DIR / ".env")
+
+# Get variables
+MONGODB_URI = os.getenv("MONGODB_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
+# Debug prints
+print("MONGODB_URI =", MONGODB_URI)
+print("DATABASE_NAME =", DATABASE_NAME)
+
+# MongoDB connection
+client = MongoClient(MONGODB_URI)
+
+db = client[DATABASE_NAME]
+
+print("MongoDB Connected Successfully")

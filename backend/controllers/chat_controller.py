1871cb9c252a06d66f11cd8562320ddc98157135
HeimerHart <s.sriansh0405@gmail.com>
2026-05-29 13:57:31 +0530

    w2 folder structure

--- backend/controllers/chat_controller.py
+++ backend/controllers/chat_controller.py
@@ -1,9 +1,38 @@
 from utils.logger import logger
+from database.mongodb import db
 
+
+intent_collection = db["intents"]
 async def process_chat(message: str):
 
     logger.info(f'User message: {message}')
 
+    message=message.lower()
+    
+    #search matching intent
+    
+    intent=intent_collection.find_one({
+        "patterns":{
+            "$in":[message]
+        }
+
+
+    })
+
+    #if intent is found
+    if intent:
+        response=intent["responses"][0]
+
+        return{
+            "intent":intent["name"],
+            "response":response
+        }
+
+
+
+
+
     return {
-        'response': f'You said: {message}'
+        "intent":"unknown",
+        'response': "i do not understant"
     }
